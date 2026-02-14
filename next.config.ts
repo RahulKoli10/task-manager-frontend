1f6aea3f6d09d7adc244a0a1f233a56b10761311
RahulKoli10 <rahulkohli7078@gmail.com>
2026-02-14 16:32:37 +0530

    ch

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+
+// export default nextConfig;
+// module.exports = {
+//   output: "export",
+// };
+
+
+
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
 };
 
-export default nextConfig;
+module.exports = nextConfig;
